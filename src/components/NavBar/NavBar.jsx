ed5a8717e38cfbae57ec1bcaef23c5991a68746f
Anibal Ayala <anibalayala1990@gmail.com>
2023-12-19 23:32:25 -0300

    pre-entrega1

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import classes from './NavBar.module.css'
+import CartWidget from '../CartWidget/CartWidget'
+
+
+const NavBar = () => {
+    return(
+        <nav>
+            <h1 className={classes.titulo}>E-Commerce</h1>
+            <section className={classes.seccion}>
+             
+                <div className={classes.boton}>
+                <button className='btn btn-warning'>
+                   Celulares
+                </button></div>
+                <div className={classes.boton}>
+                <button className='btn btn-primary'>
+                  Tablets
+                </button></div>
+
+                <div className={classes.boton}>
+                <button className='btn btn-danger'>
+                  Notebooks
+                </button></div>
+
+            </section>
+            <CartWidget/>
+        </nav>
+    )
+}
+
+export default NavBar
